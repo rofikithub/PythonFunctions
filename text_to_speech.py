3103501b16b97a0bae27873aca34f29a77cd413b
Md. Rafikul Islam (Rofik) <156637357+rofikithub@users.noreply.github.com>
2024-01-18 12:21:05 +0600

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,1 +1,17 @@
+import os
+import playsound        # pip install playsound
+from gtts import gTTS   # pip install gTTS
+
+
+def text_file_playsound(text):
+    toSpeak = gTTS(text=text, lang='bn')
+    file = "audio.mp3"
+    toSpeak.save(file)
+    playsound.playsound(file, True)
+    os.remove(file)
+    print(text)
+
+
+if __name__ == "__main__":
+    text_file_playsound("আমি আপনার জন্য কি করতে পারি ..")
 
