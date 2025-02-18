4eeeb715bc1a731037e4e9b8026e3c3c2776eb93
Md. Rafikul Islam (Rofik) <156637357+rofikithub@users.noreply.github.com>
2025-02-18 11:07:22 +0600

    Update schedule_task.py

--- schedule_task.py
+++ schedule_task.py
@@ -1,1 +1,12 @@
+# pip install schedule
+import schedule
+import time
 
+def task():
+    print("Task executed...")
+
+schedule.every(10).seconds.do(task)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
